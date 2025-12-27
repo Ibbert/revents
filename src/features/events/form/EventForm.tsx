876149bed843ad0c24e8a34600f0bfbfd2f2784f
Ibrahim <Zaid_Bn@hotmail.com>
2025-12-27 16:08:00 +0100

    End of Section 4

--- src/features/events/form/EventForm.tsx
+++ src/features/events/form/EventForm.tsx
@@ -1,46 +1,111 @@
+import { users } from "../../../lib/data/sampleData";
+import type { AppEvent } from "../../../lib/types";
+
 type Props = {
   setFormOpen: (isOpen: boolean) => void;
-}
+  createEvent: (event: AppEvent) => void;
+  selectedEvent: AppEvent | null;
+  updateEvent: (event: AppEvent) => void;
+};
 
-export default function EventForm({setFormOpen}: Props) {
+export default function EventForm({
+  setFormOpen,
+  createEvent,
+  selectedEvent,
+  updateEvent,
+}: Props) {
+  const initialValues = selectedEvent ?? {
+    title: "",
+    category: "",
+    description: "",
+    date: "",
+    city: "",
+    venue: "",
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const data = Object.fromEntries(formData.entries()) as unknown as AppEvent;
+
+    if (selectedEvent) {
+      updateEvent({ ...selectedEvent, ...data });
+      setFormOpen(false);
+      return;
+    } else {
+      createEvent({
+        ...data,
+        id: crypto.randomUUID(),
+        hostUid: users[0].uid,
+        attendees: [
+          {
+            id: users[0].uid,
+            displayName: users[0].displayName,
+            photoURL: users[0].photoURL,
+            isHost: true,
+          },
+        ],
+      });
+      setFormOpen(false);
+    }
+  };
+
   return (
     <div className="card bg-base-100 p-4 flex flex-col gap-3 w-full">
       <h3 className="text-2xl font-semibold text-center text-primary">
-        Create new event
+        {selectedEvent ? "Edit Event" : "Create New Event"}
       </h3>
-      <form className="flex flex-col gap-3 w-full">
+      <form action={onSubmit} className="flex flex-col gap-3 w-full">
         <input
+          defaultValue={initialValues.title}
+          name="title"
           type="text"
           className="input input-lg w-full"
           placeholder="Event title"
         />
         <input
+          defaultValue={initialValues.category}
+          name="category"
           type="text"
           className="input input-lg w-full"
           placeholder="Category"
         />
         <input
+          defaultValue={initialValues.description}
+          name="description"
           type="textarea"
           className="textarea textarea-lg w-full"
           placeholder="Description"
         />
         <input
-          type="date"
-          className="date date-lg w-full"
+          defaultValue={
+            initialValues.date
+              ? new Date(initialValues.date).toISOString().slice(0, 16)
+              : ""
+          }
+          name="date"
+          type="datatime-local"
+          className="input input-lg w-full"
           placeholder="Date"
         />
         <input
+          defaultValue={initialValues.city}
+          name="city"
           type="text"
           className="input input-lg w-full"
           placeholder="City"
         />
         <input
+          defaultValue={initialValues.venue}
+          name="venue"
           type="text"
           className="input input-lg w-full"
           placeholder="Venue"
         />
         <div className="flex justify-end w-full gap-3">
-          <button onClick={() => setFormOpen(false)} type="button" className="btn btn-neutral">
+          <button
+            onClick={() => setFormOpen(false)}
+            type="button"
+            className="btn btn-neutral"
+          >
             Cancel
           </button>
           <button type="submit" className="btn btn-primary">
